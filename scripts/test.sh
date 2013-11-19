c9cf0f6bae97b97188dc45f63309be11dad79ff1
Peter Sutton <foxxy@foxdogstudios.com>
2013-11-19 11:29:20 +0000

    Before running tests, ensure test server is up

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o errexit
 set -o nounset
@@ -6,6 +6,34 @@ set -o nounset
 
 repo=$(realpath "$(dirname "$(realpath -- "${BASH_SOURCE[0]}")")/..")
 cd -- "${repo}"
+
+function is_listening()
+{
+    curl http://localhost:3000/ &> /dev/null
+}
+
+if ! is_listening; then
+    echo -n 'Launching test Meteor server.'
+
+    function clean_up()
+    {
+        if [[ -v pid ]]; then
+            kill "${pid}" &> /dev/null || true
+        fi
+    }
+
+    trap clean_up EXIT
+
+    "${repo}/scripts/mrt.sh" &> /dev/null &
+    pid=${?}
+
+    until is_listening; do
+        echo -n '.'
+        sleep 0.1
+    done
+
+    echo 'done'
+fi
 
 set +o nounset
 source env/bin/activate
