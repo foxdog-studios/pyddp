c9fc0f0a31e1d8f386761138e8d824ac47dd90de
Peter Sutton <foxxy@foxdogstudios.com>
2014-05-27 13:09:23 +0100

    v0.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='pyddp',
-    version='0.1.0',
+    version='0.2.0',
     description='Distributed Data Protocol (DDP)',
     author='Peter Sutton',
     author_email='foxxy@foxdogstudios.com',
